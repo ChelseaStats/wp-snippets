40efa20d6ab70b5cdbe9f624149426c1a4f6fb4b
Mat <Git@thecellarroom.net>
2013-02-28 10:57:43 +0000

    Create listcategories.php

---
+++ listcategories.php
@@ -0,0 +1,23 @@
+/* list categories for WordPress */
+
+$args = array(
+	'show_option_all'    => '',
+	'orderby'            => 'name',
+	'order'              => 'ASC',
+	'style'              => 'list',
+	'show_count'         => 0,
+	'hide_empty'         => 1,
+	'use_desc_for_title' => 1,
+	'hierarchical'       => null,
+	'title_li'           => null,
+	'show_option_none'   => __('No categories'),
+	'number'             => null,
+	'echo'               => 1,
+	'depth'              => 0,
+	'current_category'   => 0,
+	'pad_counts'         => 0,
+	'taxonomy'           => 'category',
+	'walker'             => null
+); 
+
+wp_list_categories($args);
