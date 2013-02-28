1b8dbe32c20aae181709f1aa6efa19238383b130
Mat <Git@thecellarroom.net>
2013-02-28 10:28:22 +0000

    Update relatedposts-category.php

--- relatedposts-category.php
+++ relatedposts-category.php
@@ -2,9 +2,13 @@
 
 <ul id="square"> 
 <?php
-$related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 7, 'post__not_in' => array($post->ID) ) );
+$related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 
+                             'numberposts' => 7, 
+                             'post__not_in' => array($post->ID) ) );
 if( $related ) foreach( $related as $post ) {
 setup_postdata($post); ?>
-    <li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+
+<li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+
 <?php } wp_reset_postdata(); ?>
 </ul>  
