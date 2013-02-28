71023892669129bb41afa3322e521b2c925f3f49
Mat <Git@thecellarroom.net>
2013-02-28 10:52:51 +0000

    Create compression.php

---
+++ compression.php
@@ -0,0 +1,4 @@
+/* compressions for WordPress */
+if(extension_loaded("zlib") && (ini_get("output_handler") != "ob_gzhandler"))
+   add_action('wp', create_function('', '@ob_end_clean();
+   @ini_set("zlib.output_compression", 1);'));
