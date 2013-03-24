5b41f4b05b393cca988b80f8b17fc28341d3d5d5
kutf <git@thecellarroom.net>
2013-03-24 19:26:00 +0000

    dummy admin user with SQL

---
+++ wp-sql-add-admin-user.sql
@@ -0,0 +1,7 @@
+/* to add an a dummy admin user with SQL, ideal on a localhost from a live site database import. */
+/* change details to suit, where X is the id number (choose next number depending on count of your current users */
+
+INSERT INTO `wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`) 
+VALUES ('X', 'root, MD5('root'), author name, email@domain.tld, domain.tld', '2012-01-20 00:00:00', '', '0', My name)
+INSERT INTO `wp_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) VALUES (NULL, 'X', 'wp_capabilities', 'a:1:{s:13:"administrator";b:1;}')
+INSERT INTO `wp_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) VALUES (NULL, 'X', 'wp_user_level', '10')
