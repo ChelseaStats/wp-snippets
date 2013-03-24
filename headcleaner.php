e20ff9cad64b566eee631d73ba9e0311332c46b9
kutf <git@thecellarroom.net>
2013-03-24 20:36:21 +0000

    removed duplication and blank line

--- headcleaner.php
+++ headcleaner.php
@@ -10,4 +10,3 @@ remove_action('wp_head' ,'parent_post_rel_link',10,0);
 remove_action('wp_head' ,'start_post_rel_link',10,0); 
 remove_action('wp_head' ,'adjacent_posts_rel_link_wp_head',10,0); 
 remove_action('wp_head' ,'wp_generator'); 
-remove_action('init' ,'wp_admin_bar_init');
