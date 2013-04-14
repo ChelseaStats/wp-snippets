f2e5341b09151ef03b71a39e9f71f3fd5bfb3889
Mat <Git@thecellarroom.net>
2013-04-15 00:22:01 +0200

    Update compression.php

--- compression.php
+++ compression.php
@@ -2,3 +2,4 @@
 if(extension_loaded("zlib") && (ini_get("output_handler") != "ob_gzhandler"))
    add_action('wp', create_function('', '@ob_end_clean();
    @ini_set("zlib.output_compression", 1);'));
+// this is not compatible with wp-super cache by the way   
