f828716e37ca2ed9a6b7e746603d457c0a2b596a
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2015-07-03 15:30:51 +0100

    Update versioncheckforadmin.php

--- versioncheckforadmin.php
+++ versioncheckforadmin.php
@@ -1,8 +1,24 @@
+/*
+ Plugin Name:  Remove version checking
+ Description: Remove version check from non-admins
+*/
 /* Admin version check for WordPress */
 
+defined( 'ABSPATH' ) or die();
 
-// remove version check from non-admins
-if ( !current_user_can('administrator') ) {
-    add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
-    add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
-}
+/*************************************************************************/
+if ( ! class_exists( 'tcr_remove_version_for_nonadmins' ) ) :
+
+    class tcr_remove_version_for_nonadmins {
+    
+        function __construct() {
+            // remove version check from non-admins
+            if ( !current_user_can('administrator') ) {
+                add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
+                add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
+            }
+        }
+        
+}      
+    
+endif;
