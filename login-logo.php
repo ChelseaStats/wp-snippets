353e6f185898a3be7c812e9e0a0c5edbb23bf17d
ChelseaStats <git@thechels.co.uk>
2015-07-05 02:06:04 +0100

    new

--- login-logo.php
+++
@@ -1,8 +0,0 @@
-/* Add a custom login logo for WordPress */
-
-function tcr_login_logo() {
-    echo '<style type="text/css">
-        h1 a { background-image:url('.get_bloginfo('template_directory').'/images/login_logo.png) !important; }
-    </style>';
-}
-add_action('login_head', 'tcr_login_logo');
